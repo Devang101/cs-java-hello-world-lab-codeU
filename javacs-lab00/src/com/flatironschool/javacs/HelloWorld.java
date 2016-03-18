fa46df4c5905b081cc8a79cf348576411fe4fdb0
Devang101 <devang.patel15@schulerscholar.org>
2016-03-17 18:50:34 -0700

    first commit

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -1,11 +1,16 @@
 package com.flatironschool.javacs;
 
-public class HelloWorld {
-    public static Double getVersion() {
-        // I won't work!
+public class HelloWorld
+{
+    public static Double getVersion()
+    {
+       String print = System.getProperty("java.version").substring(0,3);
+       double version = Double.parseDouble(print);
+       return version;
     }
 
-    public static void main(String[] args) {
-	    // you can test the output of getVersion() here
+    public static void main(String[] args)
+    {
+	    getVersion();
     }
 }
